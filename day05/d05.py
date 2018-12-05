8caca2474fa6cc20363d7d1d3e2ae3efbe203fee
davecox <dave@harvestacuity.com>
2018-12-05 14:23:20 -0500

    cleanup

--- day05/d05.py
+++ day05/d05.py
@@ -1,51 +1,47 @@
 #!/usr/bin/python3
 
+def react(polymer,elements):
+    # Exit criteria
+    oldLen=-1;
+    done=False;
+    # Eliminate aA,Aa,bB,Bb, etc until no further reductions exist
+    while(not done):
+        for E in elements:
+            polymer=polymer.replace("".join( [ E, E.upper() ]),"");
+            polymer=polymer.replace("".join( [ E.upper(), E ]),"");
+        if len(polymer)==oldLen:
+            done=True;
+        else:
+            oldLen=len(polymer);
+    return oldLen;
+
 # Input data
 #input="dabAcCaCBAcCcaDA"
 with open('input.txt') as f:
     input=f.read().rstrip('\n');
-f.close()
-
-# Initiaze exit criteria
-oldLen=-1;
-done=False;
 
 # Create set of unique elements in polymer
-unique=set([c.lower() for c in input]);
+elements=set([c.lower() for c in input]);
 
-# replace aA,Aa, bB etc combos
-while(not done):
-    for U in unique:
-        input=input.replace("".join( [ U, U.upper() ]),"");
-        input=input.replace("".join( [ U.upper(), U ]),"");
-    # Check length, if stable exit reaction loop
-    if len(input)==oldLen:
-        done=True;
-    else:
-        oldLen=len(input);
-# print length of fully reacted polymer        
-print("Part A: Solution is {0:d}".format(len(input)));
+# Print solution
+print("Part A: Solution is {0:d}".format(react(input,elements)));
 
 
 # repeat process with pre-processing which removes one element
-# track length to find shortest
-lenList=list();
-for poly in unique:
-    # remove one element
-    inmod=input.replace(poly,"").replace(poly.upper(),"");
+# track length to find shortest resulting polymer
+with open('input.txt') as f:
+    input=f.read().rstrip('\n');
 
-    oldLen=-1;
-    done=False;
-    while(not done):
-        for U in unique:
-            inmod=inmod.replace("".join( [ U, U.upper() ]),"");
-            inmod=inmod.replace("".join( [ U.upper(), U ]),"");
-        if len(inmod)==oldLen:
-            done=True;
-        else:
-            oldLen=len(inmod);
-    lenList.append(oldLen);
-    #print("  Removing {} yields {} length polymer".format(poly,oldLen));
+lenList=list();
+for E in elements:
+    # remove one element
+    inmod=input.replace(E,"").replace(E.upper(),"");
+    # react and store length
+    lenList.append(react(inmod,elements));
+    # progress print...
+    print("  Removing {} yields {} length polymer".format(E,lenList[-1]));
+
+#Print solution
 print("Part B: Solution is {0:d}".format(min(lenList)));
 	
 
